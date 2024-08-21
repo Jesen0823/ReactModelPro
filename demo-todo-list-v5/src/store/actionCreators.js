080d51335832247009c04cb904d4873f4b8f80ad
Jesen0823 <xie_stacol@163.com>
2024-08-21 09:36:00 +0800

    [22] edit file description.

--- demo-todo-list-v5/src/store/actionCreators.js
+++ demo-todo-list-v5/src/store/actionCreators.js
@@ -1,3 +1,10 @@
+// 1. redux-thunk 是一个redux中间件，用来处理复杂逻辑，如异步请求
+// 2. redux-thunk中间件可以让action创建函数不仅仅返回一个action对象，也可以返回一个函数
+
+// 3. redux-saga是一个用于管理redux异步操作的中间件，通过创建sagas将所有异步操作集中管理，可用来代替redux-thunk中间件。
+// 4. saga负责协调复杂的异步操作
+// 5. reduce负责处理action的stage更新
+
 import {
     DEL_TODO_ITEM,
     CHANGE_ITEM_CHECK,
@@ -8,12 +15,6 @@ import {
 } from './actionType';
 import {getTodoList} from "../api";
 import store from "./index";
-// 1. redux-thunk 是一个redux中间件，用来处理复杂逻辑，如异步请求
-// 2. redux-thunk中间件可以让action创建函数不仅仅返回一个action对象，也可以返回一个函数
-
-// 3. redux-saga是一个用于管理redux异步操作的中间件，通过创建sagas将所有异步操作集中管理，可用来代替redux-thunk中间件。
-// 4. saga负责协调复杂的异步操作
-// 5. reduce负责处理action的stage更新
 
 export const getItemListAction = () => {
     return (dispatch)=>{
